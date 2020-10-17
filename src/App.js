3a36aafb33266492c80f1ff6e4ec9f0cc5370388
Felizardo <devfelizardo@gmail.com>
2020-10-16 22:54:46 -0300

    using states and effects of react

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import React from "react";
 const API_RANDOM_JOKE = 'http://api.icndb.com/jokes/random';
 
 const App = () => {
+  const [joke, setJoke] = React.useState('');
+
+  const generateJoke = () => {
+    fetch(API_RANDOM_JOKE)
+    .then(res => res.json())
+    .then(data => setJoke(data.value.joke))
+  }
+
+  React.useEffect(() => {
+    generateJoke();
+  }, [])
+
   return (
     <div className="box"> 
-      <h3>Another Chuck Norris Joke Generator</h3>
-      <p>Joke Here!!!</p>
-      <button>Generate New Joke!</button>
+      <h3>Not Enough Chuck Norris Jokes!</h3>
+      <p dangerouslySetInnerHTML={{__html: joke}}/>
+      <button onClick={generateJoke}>Generate New Joke</button>
     </div>
     );
 };
